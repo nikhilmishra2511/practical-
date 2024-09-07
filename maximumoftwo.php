2e788e59840934c88631853d735dc9b5db9a9d70
nikhilmishra2511 <154616820+nikhilmishra2511@users.noreply.github.com>
2024-09-07 22:37:32 +0530

    Add files via upload

---
+++ maximumoftwo.php
@@ -0,0 +1,13 @@
+<?php
+
+//writer :- NIKHIL MISHRA
+//Date   :- 22/08/2024
+
+$a=50;
+$b=60;
+if($a>$b)
+  $max=$a;
+else
+  $max=$b;
+echo"$max is largest";
+?>
---
+++ star.php
@@ -0,0 +1,20 @@
+<?php
+
+
+// Writer: NIKHIL MISHRA
+// Date: 22/08/2024
+
+
+$lines = 3;
+
+
+for ($i = 1; $i <= $lines; $i++) {
+
+    for ($j = 1; $j <= $i; $j++) {
+        echo "*";
+    }
+
+    echo "\n";
+}
+
+?>
